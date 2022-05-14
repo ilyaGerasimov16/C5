8d70bcfd2704904b01e95e9aba4d60eee5c0227a
Ilya Gerasimov <ilya.gerasimov.97@mail.ru>
2022-05-14 19:56:12 +0300

    Создание интерфейса LoginApi и его имплементаций - Web и Mock

---
+++ app/src/main/java/com/example/c5/MockLoginApi.kt
@@ -0,0 +1,19 @@
+package com.example.c5
+
+class MockLoginApi:LoginApi {
+    override fun login(login: String, password: String): Boolean {
+        return login==password
+    }
+
+    override fun register(login: String, password: String, email: String): Boolean {
+        return login.isNotEmpty() && password.isNotEmpty()
+    }
+
+    override fun logout(): Boolean {
+        return true
+    }
+
+    override fun forgotPassword(login: String): Boolean {
+        return true
+    }
+}
---
+++ app/src/main/java/com/example/c5/WebLoginApiImpl.kt
@@ -0,0 +1,19 @@
+package com.example.c5
+
+class WebLoginApiImpl:LoginApi {
+    override fun login(login: String, password: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun register(login: String, password: String, email: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun logout(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun forgotPassword(login: String): Boolean {
+        TODO("Not yet implemented")
+    }
+}
